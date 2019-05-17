992b318f458ffa23e09195c2b523e500c3fecf34
Emmauel Carbellido <ixiasuraixi@gmail.com>
2019-05-17 10:45:17 -0500

    Primer Commit

---
+++ src/main/java/com/decode/java/WebPageMvcApplication.java
@@ -0,0 +1,13 @@
+package com.decode.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPageMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPageMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/decode/java/WebPageMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.decode.java;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebPageMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
